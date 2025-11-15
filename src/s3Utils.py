e8e245eb0600d15af7adad546e010f8e819727e8
akaplan521 <alexa.kaplan@uvm.edu>
2025-11-14 19:46:44 -0500

    util functions

--- src/s3Utils.py
+++ src/s3Utils.py
@@ -1,9 +1,17 @@
 import boto3
 
-s3 = boto3.client('s3')
+#creating s3 client
+def get_s3_client(region="us-east-2"):
+    return boto3.client("s3", regionName=region)
 
-s3.upload_file(
-    Filename='test.txt',
-    Bucket='nba-sentiment-data',
-    Key='test.txt'
-)
+#uploading files to bucket
+def upload_file(filename, bucket, key, region="us-east-2"):
+    s3 = get_s3_client(region)
+    s3.upload_file(Filename=filename, Bucket=bucket, Key=key)
+    print(f"Uploaded {filename} to s3://{bucket}/{key}")
+
+#dowloading files from bucket
+def download_file(bucket, key, filename, region="us-east-2"):
+    s3 = get_s3_client(region)
+    s3.download_file(Bucket=bucket, Key=key, Filename=filename)
+    print(f"Downloaded s3://{bucket}/{key} to {filename}")
