4a2584b6a0da9a4e5a7db893fc88097f688c1d93
akaplan521 <alexa.kaplan@uvm.edu>
2025-11-22 01:48:35 -0500

    ohhhhh so there are actually no APIs to use

--- src/elt/ingest_reddit.py
+++ src/elt/ingest_reddit.py
@@ -1,11 +1,12 @@
 import os
 import json
-import praw
 import pandas
 import sys
+import requests
 from datetime import date 
-#add src folder to system path so we can import s3Utils
+#add src folder to system path so I can import s3Utils
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import s3Utils as util #figure out how to import in dif folder
+import s3Utils as util 
 
 #use date.today() for day in yyyy-mm-dd
+
